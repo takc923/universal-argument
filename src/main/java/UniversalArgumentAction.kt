e3533ffe43041f3417c2e92b69a0a84362696f47
Takeshi Kunimi <takeshi.kunimi@umitron.com>
2019-03-17 19:43:51 +0900

    Support more action

--- src/main/java/UniversalArgumentAction.kt
+++ src/main/java/UniversalArgumentAction.kt
@@ -1,6 +1,7 @@
 import com.intellij.codeInsight.editorActions.PasteHandler
 import com.intellij.codeInsight.hint.HintManager
 import com.intellij.codeInsight.template.impl.editorActions.TypedActionHandlerBase
+import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.actionSystem.IdeActions
 import com.intellij.openapi.editor.Caret
@@ -119,67 +120,29 @@ class UniversalArgumentAction : EditorAction(Handler()) {
                     else myOriginalHandler.execute(editor, caret, dataContext)
 
             private fun registerActions() {
-                val actionManager = EditorActionManager.getInstance()
+                val editorActionManager = EditorActionManager.getInstance()
 
-                val typedAction = actionManager.typedAction
+                val typedAction = editorActionManager.typedAction
                 typedAction.setupRawHandler(MyTypedHandler(typedAction.rawHandler))
 
-                for (action in supportedActions) {
-                    val handler = actionManager.getActionHandler(action)
-                    when (handler) {
-                        is EditorWriteActionHandler -> actionManager.setActionHandler(action, MyEditorWriteActionHandler(handler))
-                        is PasteHandler -> actionManager.setActionHandler(action, MyPasteActionHandler(handler))
-                        else -> actionManager.setActionHandler(action, MyEditorActionHandler(handler))
-                    }
-                }
-                actionManager.setActionHandler(IdeActions.ACTION_EDITOR_ESCAPE, MyEscapeEditorActionHandler(actionManager.getActionHandler(IdeActions.ACTION_EDITOR_ESCAPE)))
-            }
+                val actionManager = ActionManager.getInstance()
 
-            private val supportedActions = listOf(
-                    IdeActions.ACTION_EDITOR_CUT,
-                    IdeActions.ACTION_EDITOR_PASTE,
-                    IdeActions.ACTION_EDITOR_PASTE_SIMPLE,
-                    IdeActions.ACTION_EDITOR_DELETE,
-                    IdeActions.ACTION_EDITOR_DELETE_TO_WORD_START,
-                    IdeActions.ACTION_EDITOR_DELETE_TO_WORD_END,
-                    IdeActions.ACTION_EDITOR_DELETE_LINE,
-                    IdeActions.ACTION_EDITOR_ENTER,
-                    IdeActions.ACTION_EDITOR_START_NEW_LINE,
-                    IdeActions.ACTION_EDITOR_SPLIT,
-                    IdeActions.ACTION_EDITOR_FORWARD_PARAGRAPH,
-                    IdeActions.ACTION_EDITOR_BACKWARD_PARAGRAPH,
-                    IdeActions.ACTION_EDITOR_SELECT_WORD_AT_CARET,
-                    IdeActions.ACTION_EDITOR_UNSELECT_WORD_AT_CARET,
-                    IdeActions.ACTION_EDITOR_BACKSPACE,
-                    IdeActions.ACTION_EDITOR_MOVE_CARET_LEFT_WITH_SELECTION,
-                    IdeActions.ACTION_EDITOR_MOVE_CARET_RIGHT_WITH_SELECTION,
-                    IdeActions.ACTION_EDITOR_MOVE_CARET_UP_WITH_SELECTION,
-                    IdeActions.ACTION_EDITOR_MOVE_CARET_DOWN_WITH_SELECTION,
-                    IdeActions.ACTION_EDITOR_SWAP_SELECTION_BOUNDARIES,
-                    IdeActions.ACTION_EDITOR_MOVE_CARET_UP,
-                    IdeActions.ACTION_EDITOR_MOVE_CARET_LEFT,
-                    IdeActions.ACTION_EDITOR_MOVE_CARET_DOWN,
-                    IdeActions.ACTION_EDITOR_MOVE_CARET_RIGHT,
-                    IdeActions.ACTION_EDITOR_MOVE_CARET_PAGE_UP,
-                    IdeActions.ACTION_EDITOR_MOVE_CARET_PAGE_DOWN,
-                    IdeActions.ACTION_EDITOR_MOVE_CARET_PAGE_UP_WITH_SELECTION,
-                    IdeActions.ACTION_EDITOR_MOVE_CARET_PAGE_DOWN_WITH_SELECTION,
-                    IdeActions.ACTION_EDITOR_NEXT_WORD,
-                    IdeActions.ACTION_EDITOR_PREVIOUS_WORD,
-                    IdeActions.ACTION_EDITOR_NEXT_WORD_WITH_SELECTION,
-                    IdeActions.ACTION_EDITOR_PREVIOUS_WORD_WITH_SELECTION,
-                    IdeActions.ACTION_EDITOR_TAB,
-                    IdeActions.ACTION_EDITOR_JOIN_LINES,
-                    IdeActions.ACTION_EDITOR_COMPLETE_STATEMENT,
-                    IdeActions.ACTION_EDITOR_CLONE_CARET_BELOW,
-                    IdeActions.ACTION_EDITOR_CLONE_CARET_ABOVE,
-                    IdeActions.ACTION_EDITOR_NEXT_PARAMETER,
-                    IdeActions.ACTION_EDITOR_PREV_PARAMETER,
-                    IdeActions.ACTION_EDITOR_NEXT_TEMPLATE_VARIABLE,
-                    IdeActions.ACTION_EDITOR_PREVIOUS_TEMPLATE_VARIABLE,
-                    IdeActions.ACTION_EDITOR_DUPLICATE,
-                    IdeActions.ACTION_EDITOR_DUPLICATE_LINES
-            )
+                for (actionId in actionManager.getActionIds("")) {
+                    val action = actionManager.getAction(actionId) as? EditorAction ?: continue
+                    val handler = action.handler
+                    val newHandler = when {
+                        action is UniversalArgumentAction -> null
+                        actionId == IdeActions.ACTION_EDITOR_ESCAPE -> MyEscapeEditorActionHandler(handler)
+                        handler is EditorWriteActionHandler -> MyEditorWriteActionHandler(handler)
+                        handler is PasteHandler -> MyPasteActionHandler(handler)
+                        handler is EditorActionHandler -> MyEditorActionHandler(handler)
+                        else -> null
+                    }
+                    newHandler ?: continue
+
+                    editorActionManager.setActionHandler(actionId, newHandler)
+                }
+            }
         }
     }
 }
